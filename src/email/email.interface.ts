2e78f7549d418b50fad64141ad16b5d5716c71ba
zhangcf <vite@88.com>
2025-10-29 16:11:31 +0800

    feat(新增动态使用配置发送邮件): ✨

--- src/email/email.interface.ts
+++ src/email/email.interface.ts
@@ -1,8 +1,14 @@
-export interface EmailModuleOptions {
+export interface EmailConfig {
     host: string;
     port: number;
     user: string;
     pass: string;
     fromName?: string;
     fromEmail?: string;
+}
+
+export interface EmailSendOptions {
+    to: string
+    subject: string
+    html: string
 }
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,29 +1,30 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 
 import { createTransport, Transporter } from 'nodemailer';
-import { EmailModuleOptions } from './email.interface';
+import { EmailConfig, EmailSendOptions } from './email.interface';
 import { MODULE_OPTIONS_TOKEN } from './email.options';
 @Injectable()
 export class EmailService {
     transporter: Transporter;
 
-    constructor(@Inject(MODULE_OPTIONS_TOKEN) private config: EmailModuleOptions) {
-        this.transporter = createTransport({
-            host: this.config.host,
-            port: this.config.port,
+    constructor(@Inject(MODULE_OPTIONS_TOKEN) private config: EmailConfig) {
+        this.transporter = this.getTransporter(config)
+    }
+
+    /** 创建发送器*/
+    getTransporter(config:EmailConfig){
+        return createTransport({
+            host: config.host,
+            port: config.port,
             secure: false,
             auth: {
-                user: this.config.user,
-                pass: this.config.pass,
+                user: config.user,
+                pass: config.pass,
             },
         });
     }
 
-    async sendMain({ to, subject, html }: {
-        to: string
-        subject: string
-        html: string
-    }) {
+    async sendMain({ to, subject, html }:EmailSendOptions) {
         // 判断是否发送方和接收方地址相同
         const address = this.config.user;
         if (to == address) {
@@ -32,6 +33,25 @@ export class EmailService {
         await this.transporter.sendMail({
             from: {
                 name: this.config.fromName || "Nest-Box-Mail",
+                address,
+            },
+            to,
+            subject,
+            html,
+        });
+    }
+
+    /** 动态调用发送方发送邮件*/
+    async sendWithConfig(config:EmailConfig,email:EmailSendOptions){
+        const transporter = this.getTransporter(config)
+        const {user:address} = config
+        const { to, subject, html } = email
+        if (to == address) {
+            throw new BadRequestException('邮件地址与发送方相同');
+        }
+        await transporter.sendMail({
+            from: {
+                name:'',
                 address,
             },
             to,
